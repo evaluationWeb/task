6b7eb2ee4738556ac03149baee31fb408135440f
mithridatem <mathieumithridate@adrar-formation.com>
2025-07-29 15:21:42 +0200

    fix : arborescence

--- utils/utilitaire.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//Supprimer les balises + les caractères spéciaux, suppression des espaces
-function sanitize(string $value) {
-    
-    return htmlspecialchars(strip_tags(trim($value)), ENT_NOQUOTES);
-}
