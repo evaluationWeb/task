41f02cf36f0660bc65803d18f27e8bc60fb87fb2
mithridatem <mathieumithridate@adrar-formation.com>
2025-08-12 21:43:58 +0200

    feat : update navbar icon user et logout

--- App/View/components/navbar.php
+++ App/View/components/navbar.php
@@ -10,7 +10,8 @@
         <li><a href="<?= BASE_URL ?>/category/add">Ajouter catégorie</a></li>
         <li><a href="<?= BASE_URL ?>/task/all">Liste taches</a></li>
         <li><a href="<?= BASE_URL ?>/task/add">Ajouter une tache</a></li>
-        <li><a href="<?= BASE_URL ?>/user/deconnexion"><img src="<?= BASE_URL ?>/public/image/<?= $_SESSION["img"] ?>" alt="deconnexion"></a></li>
+        <li><a href="<?= BASE_URL ?>/user/deconnexion"><img src="<?= BASE_URL ?>/public/image/<?= $_SESSION["img"] ?>" alt="image de profil" title="profil"></a></li>
+        <li><a href="<?= BASE_URL ?>/user/profil"><img src="<?= BASE_URL ?>/public/image/logout.png" alt="deconnexion" title="deconnexion"></a></li>
         <?php else : ?>
         <!-- Menu déconnecté -->
         <li><a href="<?= BASE_URL ?>/user/register">Inscription</a></li>
