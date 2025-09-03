ab1258d20c59345017fd4a4952a56ec2882fdf0a
mithridatem <mathieumithridate@adrar-formation.com>
2025-09-03 12:20:01 +0200

    feat : suppression des dépendances Bdd dans le Model

--- App/Model/Category.php
+++ App/Model/Category.php
@@ -2,23 +2,13 @@
 
 namespace App\Model;
 
-use App\Utils\Bdd;
-use App\Model\CategoryException;
+
 
 class Category
 {
     //Attributs
     private int $idCategory;
     private string $name;
-
-    //Bdd (récupérer la connexion)
-    private \PDO $connexion;
-
-    public function __construct()
-    {
-        //Injection de dépendance
-        $this->connexion = (new Bdd())->connectBDD();
-    }
 
     //Getters et Setters
     public function getIdCategory(): int
